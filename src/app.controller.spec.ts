d134f479dc80b3c82d82213ec457ec555e6eadb2
Christian Wendt <christian.wendt@xealth.io>
2023-01-03 12:51:48 -0800

    test change

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// comment
+
 describe('AppController', () => {
   let appController: AppController;
 
